8f973066e9f5d7d931042751a44aea136c48cbb8
keeles <kyleeeles@gmail.com>
2024-10-15 23:14:37 -0700

    fixing nav breakpoint for mobile

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -4,37 +4,39 @@ import logoSVG from "../assets/logo-new.svg";
 
 export default function Header() {
   return (
-    <header className="flex justify-between w-full items-center py-5 max-w-3xl mx-auto">
-      <div className="flex items-center">
-        <Link href="/">
-          <Image src={logoSVG} alt="Reach Out Logo" width={60} height={60} className="mr-2" />
-        </Link>
-        <h1 className="text-2xl">Reach Out</h1>
+    <header className="flex justify-between w-full items-center py-5 px-2 flex-col">
+      <div className="flex justify-between w-full max-w-3xl py-5 flex-col sm:flex-row">
+        <div className="flex items-center self-start py-2 px-1">
+          <Link href="/">
+            <Image src={logoSVG} alt="Reach Out Logo" width={60} height={60} className="mr-2" />
+          </Link>
+          <h1 className="text-2xl">Reach Out</h1>
+        </div>
+        <nav className="flex items-center px-2 self-center">
+          <ul className="flex space-x-5">
+            <li>
+              <Link href="/" className="font-bold hover:text-blue-600">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link href="/contact" className="font-bold hover:text-blue-600">
+                Contact
+              </Link>
+            </li>
+            <li>
+              <Link href="/blog" className="font-bold hover:text-blue-600">
+                Blog
+              </Link>
+            </li>
+            <li>
+              <Link href="/team" className="font-bold hover:text-blue-600">
+                Team
+              </Link>
+            </li>
+          </ul>
+        </nav>
       </div>
-      <nav className="flex items-center">
-        <ul className="flex space-x-5">
-          <li>
-            <Link href="/" className="font-bold hover:text-blue-600">
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link href="/contact" className="font-bold hover:text-blue-600">
-              Contact
-            </Link>
-          </li>
-          <li>
-            <Link href="/blog" className="font-bold hover:text-blue-600">
-              Blog
-            </Link>
-          </li>
-          <li>
-            <Link href="/team" className="font-bold hover:text-blue-600">
-              Team
-            </Link>
-          </li>
-        </ul>
-      </nav>
     </header>
   );
 }
