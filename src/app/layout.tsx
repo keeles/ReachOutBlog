d5070bc7c7ab56b903fcb50aec4127e121fe637f
keeles <kyleeeles@gmail.com>
2024-10-15 22:44:36 -0700

    build error fixing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,9 @@
 import type {Metadata} from "next";
-import localFont from "next/font/local";
 import {Roboto} from "next/font/google";
 import "./globals.css";
 import Header from "./components/header";
 import Footer from "./components/footer";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 const roboto = Roboto({
   subsets: ["latin"],
   display: "swap",
