c2f71df779dc7337a7f59052ee98d406b21f52dd
keeles <kyleeeles@gmail.com>
2024-11-07 15:16:12 -0800

    fix image path header

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
-import logoSVG from "../assets/logo-new.svg";
+import logoSVG from "../../assets/logo-new.svg";
 
 export default function Header() {
   return (
